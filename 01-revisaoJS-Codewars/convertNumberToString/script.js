ec4b6a8278560aa63c689762e71a56da7c55a93d
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2024-02-02 13:25:02 -0300

    ex01 - convert Number to string

---
+++ 01-revisaoJS-Codewars/convertNumberToString/script.js
@@ -0,0 +1,7 @@
+function numberToString(num) {
+  return num.toString();
+}
+
+console.log(typeof numberToString(67));
+console.log(typeof numberToString(15));
+console.log(typeof numberToString(20));
