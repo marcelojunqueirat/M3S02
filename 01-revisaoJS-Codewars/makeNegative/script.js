1c769c1d0dc104db9e0bd10be1c7fcbd9bb20966
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2024-02-02 13:44:09 -0300

    ex01 - make negative

---
+++ 01-revisaoJS-Codewars/makeNegative/script.js
@@ -0,0 +1,8 @@
+function makeNegative(num) {
+  return Math.abs(num) * -1;
+}
+
+console.log(makeNegative(42));
+console.log(makeNegative(1));
+console.log(makeNegative(99));
+console.log(makeNegative(125));
