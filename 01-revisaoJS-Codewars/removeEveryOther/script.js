2db828b78b1f9164f46e37f631eeb69698a40f5b
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2024-02-02 13:57:22 -0300

    ex01 - remove every other

---
+++ 01-revisaoJS-Codewars/removeEveryOther/script.js
@@ -0,0 +1,11 @@
+function removeEveryOther(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    arr.splice(i, 1);
+  }
+  return arr;
+}
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']));
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(removeEveryOther([[1, 2]], [[1, 2]]));
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
